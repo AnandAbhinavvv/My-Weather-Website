fe5d1a77d5a4a27bb2ce628d5442ea58154f18a3
Abhinav Anand <abhianand2414@gmail.com>
2023-09-03 14:56:34 +0530

    Completed project

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0ed3bace8c8b1ab6b6ea1b47d75d2f3b",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
